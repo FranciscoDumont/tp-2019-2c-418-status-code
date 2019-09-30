296185042aeb3d83f01b2e604cb5be2b8589b809
ailin.venerus <ailin.venerus@despegar.com>
2019-09-30 18:18:02 -0300

    implementing muse_alloc

--- Memoria/src/Muse/main.c
+++ Memoria/src/Muse/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "libmuse.h"
 
 int main() {
     printf("Hello, World!\n");
+    printf(&muse_alloc);
+
     return 0;
 }
--- Memoria/src/libMuse/libmuse.c
+++ Memoria/src/libMuse/libmuse.c
@@ -1,4 +1,14 @@
-//
-// Created by ailinvenerus on 30/9/19.
-//
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 
+/**
+     * Reserva una porción de memoria contígua de tamaño `tam`.
+     * @param tam La cantidad de bytes a reservar.
+     * @return La dirección de la memoria reservada.
+     */
+uint32_t muse_alloc(uint32_t tam) {
+    uint32_t *concat = malloc(sizeof(tam));
+    return *concat;
+}
