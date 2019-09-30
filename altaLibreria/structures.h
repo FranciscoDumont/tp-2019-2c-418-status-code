ecfdf6f09f314635d50f058664d35d393ea6b47e
Ezequiel Veliz <ezequiel_veliz@hotmail.com>
2019-09-30 00:07:27 -0300

    Libreria de comunicaciones "funcionando"

---
+++ altaLibreria/structures.h
@@ -0,0 +1,25 @@
+#ifndef DALIBRARY_STRUCTURES_H_
+#define DALIBRARY_STRUCTURES_H_
+#define IP_LENGTH 20
+
+#include "connections.h"
+
+typedef enum _NetworkDebugLevel {
+	NW_NO_DISPLAY,
+	NW_NETWORK_ERRORS,
+	NW_ALL_DISPLAY
+} NetworkDebugLevel;
+NetworkDebugLevel NETWORK_DEBUG_LEVEL = NW_NO_DISPLAY;
+
+typedef enum _MessageType {
+	ABC
+	//TODO: Agregar nuestros headers
+
+} MessageType;
+
+typedef struct _MessageHeader {
+	MessageType type;
+	int data_size;
+} MessageHeader;
+
+#endif /* DALIBRARY_STRUCTURES_H_ */
