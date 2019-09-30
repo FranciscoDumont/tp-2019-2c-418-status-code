2780c4cf7ee066662476c31ccb89c8aa6c9c32c5
Ezequiel Veliz <ezequiel_veliz@hotmail.com>
2019-09-30 11:29:27 -0300

    Ahora la libreria es instalable en el sistema

--- altaLibreria/cmake-build-debug/cmake_install.cmake
+++ altaLibreria/cmake-build-debug/cmake_install.cmake
@@ -32,6 +32,45 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  if(EXISTS "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so"
+         RPATH "")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/lib/libaltaLibreria.so")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/usr/lib" TYPE SHARED_LIBRARY FILES "/home/utnso/CLionProjects/tp-2019-2c-418-status-code/altaLibreria/cmake-build-debug/libaltaLibreria.so")
+  if(EXISTS "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/usr/lib/libaltaLibreria.so")
+    endif()
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/include/altaLibreria/connections.h;/usr/include/altaLibreria/structures.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/usr/include/altaLibreria" TYPE FILE FILES
+    "/home/utnso/CLionProjects/tp-2019-2c-418-status-code/altaLibreria/connections.h"
+    "/home/utnso/CLionProjects/tp-2019-2c-418-status-code/altaLibreria/structures.h"
+    )
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
