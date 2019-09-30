2319ded27fe105a65d13f034a7178d7f4345b0a2
Ezequiel Veliz <ezequiel_veliz@hotmail.com>
2019-09-30 15:06:42 -0300

    bla

--- Ejemplos/servidor/servidor.c
+++ Ejemplos/servidor/servidor.c
@@ -5,8 +5,8 @@
 #include "servidor.h"
 
 int main(){
-    printf("Modulo que hace cosas locas");
-    printf("Creo un hilo que al que le voy a pasar una funcion que inicia un servidor y se queda esperando por clientes");
+    printf("Modulo que hace cosas locas\n");
+    printf("Creo un hilo que al que le voy a pasar una funcion que inicia un servidor y se queda esperando por clientes\n");
     //pthread_t hiloLoco;
     //serverStart(&hiloLoco);
     serverFunction();
@@ -16,22 +16,24 @@ int main(){
 void serverFunction(){
     int socket;
     if((socket = create_socket()) == -1) {
+        printf("Error al crear el socket");
         return;
     }
     if((bind_socket(socket, 4444)) == -1) {
+        printf("Error al bindear el socket");
         return;
     }
     void new(int fd, char * ip, int port){
-        printf("Cliente conectado...\n");
+        printf("Cliente conectado, IP:%s\n", ip);
+    }
+
+    void lost(int fd, char * ip, int port){}
+    void incoming(int fd, char * ip, int port, MessageHeader * headerStruct){
         printf("Esperando mensaje...\n");
-        MessageHeader headerStruct;
-        if(receive_header(socket, &headerStruct) == -1){
-            return;
-        }
-        int size = headerStruct.data_size;
-        MessageType header = headerStruct.type;
+        int size = headerStruct->data_size;
+        MessageType header = headerStruct->type;
         char *message = malloc(sizeof(char)*size);
-        if(receive_data(socket, message, size) == -1){
+        if(receive_data(fd, message, size) == -1){
             return;
         }
         printf("El mensaje recibido es.. %s\n", message);
@@ -42,8 +44,5 @@ void serverFunction(){
         }
         free(message);
     }
-
-    void lost(int fd, char * ip, int port){}
-    void incoming(int fd, char * ip, int port, MessageHeader * header){}
     start_server(socket, &new, &lost, &incoming);
 }
