6ca9384750e6d2ffae4ce2fb7b82d8e6840eb527
Fran <franciscodumont1@gmail.com>
2019-10-05 18:04:34 -0300

    Memoria Leo configuracion

--- Memoria/src/Muse/muse.c
+++ Memoria/src/Muse/muse.c
@@ -1,11 +1,29 @@
 #include "muse.h"
 
+
 int main() {
     uint32_t tam = 2;
     //uint32_t *dir = muse_alloc(tam);
     printf("Estoy imprimiendo el valor de tam: %d\n", tam);
     //printf("Estoy imprimiendo el valor de dir: %d\n", dir);
     //muse_free(&dir);
+    read_memory_config();
 
     return 0;
+}
+
+
+void read_memory_config(){
+    config_file = config_create("config");
+
+    config.listen_port = config_get_int_value(config_file, "LISTEN_PORT");
+    config.memory_size = config_get_int_value(config_file, "MEMORY_SIZE");
+    config.page_size = config_get_int_value(config_file, "PAGE_SIZE");
+    config.swap_size = config_get_int_value(config_file, "SWAP_SIZE");
+
+    printf("LISTEN_PORT: %d\nMEMORY_SIZE: %d\nPAGE_SIZE: %d\nSWAP_SIZE: %d",\
+        config.listen_port, \
+        config.memory_size, \
+        config.page_size, \
+        config.swap_size);
 }
--- Memoria/src/Muse/muse.h
+++ Memoria/src/Muse/muse.h
@@ -5,8 +5,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
 
 #ifndef TP_2019_2C_418_STATUS_CODE_MUSE_H
 #define TP_2019_2C_418_STATUS_CODE_MUSE_H
+
+// Configuraciones
+typedef struct MEMConfig{
+    int listen_port;
+    int memory_size;
+    int page_size;
+    int swap_size;
+} MEMConfig;
+
+t_config * config_file;
+MEMConfig config;
+
+void read_memory_config();
+
 
 #endif //TP_2019_2C_418_STATUS_CODE_MUSE_H
