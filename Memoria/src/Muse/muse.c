a9afd5ed4e1a0e89869a428a250a0d4a02aae41c
Fran <franciscodumont1@gmail.com>
2019-10-05 13:11:21 -0300

    Memoria Agrego makefile

---
+++ Memoria/src/Muse/muse.c
@@ -0,0 +1,11 @@
+#include "muse.h"
+
+int main() {
+    uint32_t tam = 2;
+    //uint32_t *dir = muse_alloc(tam);
+    printf("Estoy imprimiendo el valor de tam: %d\n", tam);
+    //printf("Estoy imprimiendo el valor de dir: %d\n", dir);
+    //muse_free(&dir);
+
+    return 0;
+}
---
+++ Memoria/src/Muse/muse.h
@@ -0,0 +1,12 @@
+//
+// Created by utnso on 05/10/19.
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifndef TP_2019_2C_418_STATUS_CODE_MUSE_H
+#define TP_2019_2C_418_STATUS_CODE_MUSE_H
+
+#endif //TP_2019_2C_418_STATUS_CODE_MUSE_H
