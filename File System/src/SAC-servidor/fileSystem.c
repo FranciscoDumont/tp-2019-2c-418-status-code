fea12412664abf733c1167e8f7cfd04d8e74c3ae
emi <emiliano.ortiz@hotmail.com>
2019-10-28 17:52:04 -0300

    Mas despacio cerebrito

--- File System/src/SAC-servidor/fileSystem.c
+++ File System/src/SAC-servidor/fileSystem.c
@@ -5,56 +5,10 @@
  *
  */
 
-/*Dudas
- * Como guardas algo en un blocke?
- *
- *
- *
- *
- */
 
 
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <commons/bitarray.h>
-#include <memory.h>
-
-#define  IDENTIFICADOR_TAMANIO 3
-#define  NOMBRE_ARCHIV_MAX 71
-#define  PUNTEROS_SIMPLES_CANTIDAD 1000
-#define  RELLENO_TAMANIO 4081
-
-#define BLOQUE_TAMANIO 4096
-#define CANTIDAD_ARCHIVOS_MAX 1024
-
-typedef struct bloque_t{
-    unsigned char bytes[BLOQUE_TAMANIO];
-} GBloque;
-
-typedef struct header_t{
-    unsigned char identificador[IDENTIFICADOR_TAMANIO];
-    int32_t version;
-    int32_t bitmap_inicio;
-    int32_t bitMap_tamanio;
-    unsigned char relleno[RELLENO_TAMANIO];
-}GHeader;
-
-typedef struct file_t{
-    uint8_t estado; // 0:borrado 1:Ocupado 2:Directorio
-    unsigned char nombre_archvio[NOMBRE_ARCHIV_MAX];
-    int32_t ptr_bloque_padre;
-    int32_t size;
-    int64_t fecha_creacion;
-    int64_t fecha_modificacion;
-    int32_t GBloque[PUNTEROS_SIMPLES_CANTIDAD];
-}GFile;
+#include "fileSystem.h"
 
 int obtenerTamanioArchivo (char* file ){
     FILE* fd = fopen(file,"r");
